2c317d87b7c1125566e0988a8281b39a3bb5d843
CaFriesen <c.friesen@student.fontys.nl>
2024-07-02 20:52:10 +0200

    Added a legacy mode called basic interaction

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,14 @@
 #define DATA_PIN 16
 #define COLOR_ORDER GRB
 #define CHIPSET  WS2811
+#define COOLDOWN_TIME_MOLE 1000
+#define MAX_COMETS 50
+#define COMET_SIZE 40
+#define SENSOR_THRESHOLD 400
 
 #define SDA_1 21
 #define SCL_1 22
+
 
 ADS1115 Sensor(0x48);
 
@@ -17,12 +22,16 @@ int moleActive;
 int notPopped;
 long long popUpTimeStamp;
 long long popTime;
+long long cooldown_timer_mole;
+int first_time;
+
 
 CRGB leds[NUM_LEDS*3] = {0};
+CHSV HSV_leds[NUM_LEDS*3];
 
 CLEDController* led_controller; 
 
-enum STATE {IDLE, GAME_START, POPPING_MOLES, ADD_SCORE};
+enum STATE {IDLE, GAME_START, POPPING_MOLES, ADD_SCORE, BASIC_INTERACTION};
 STATE state;
 
 int animation_array_construction(CRGB *led_array){
@@ -42,7 +51,7 @@ void resetMole(){
 int idle()
 {
   
-  CRGB* led_head = &leds[NUM_LEDS];;
+  CRGB* led_head = &leds[NUM_LEDS];
 	long long led_timer = micros();
 
   for(int i = 0; i < NUM_LEDS; i++){
@@ -57,43 +66,36 @@ int idle()
 	//mooie idle animatie die checkt op activiteit
 }
 
-int idle2()
-{
-  CRGB* led_head;
-  if (sensor_value > 100)
+// Reads the sensor value and compares it to a threshold value. When the threshold has been surpassed and the cooldown timer is not active the a cooldown timer will be set to the defined COOLDOWN_TIME. When the cooldown timer is active the function returns true00
+int readIfMoleHit(int threshold){
+  int ADC_value = abs(Sensor.readADC_Differential_0_1());
+  Serial.println(ADC_value);
+  if (ADC_value > threshold && cooldown_timer_mole < millis())
   {
-    led_head = &leds[NUM_LEDS];
-  } 
-  else
-  {
-    led_head = &leds[0];
+    cooldown_timer_mole = millis() + COOLDOWN_TIME_MOLE;
   }
-  
-  led_controller->setLeds(led_head, NUM_LEDS);
-  FastLED.show();
-  delay(100);
 
-  return 0;
-}
-
-int readIfMoleHit(){
-  sensor_value = abs(Sensor.readADC(0));
-  return sensor_value;
+  if (cooldown_timer_mole > millis())
+  {
+    return true;
+  }
+  return false;
 }
 
 int gameStart()
 {
 	//zet 1 licht aan en wacht op de gebruiker, als dit te lang duurt terug naar idle
+  return 0;
 }
 
 int popingMoles()
 {
-  if(readIfMoleHit() && notPopped){
+  if(readIfMoleHit(SENSOR_THRESHOLD) && notPopped){
     moleActive = false;
     notPopped = false;
     popTime = millis() - popUpTimeStamp;
   }
-
+  return 0;
   
 
 	//hoofdstate van het spel, mollen komen omhoog en wachten om gesmackt te worden.
@@ -102,12 +104,59 @@ int popingMoles()
 int addScore()
 {
 	//als de speler succesvol is in het smacken van de mole wordt deze score toegevoegd aan de score verwerkt in de ledstrips
+  return 0;
+}
+
+void basicInteraction()
+{
+  if(readIfMoleHit(SENSOR_THRESHOLD) && first_time)
+  {
+    HSV_leds[0] = CHSV(random(256),0,200);
+    leds[0] = CRGB::White;
+    first_time = false;
+  }
+  else
+  {
+    first_time = true;
+  }
+}
+
+void updateLedstrip()
+{
+  CRGB* led_head = &leds[0];
+
+  switch (state)
+  {
+  case BASIC_INTERACTION:
+    for (int32_t i = 0; i < NUM_LEDS; i++)
+    {
+      if (leds[i] == CRGB::White)
+      {
+        leds[i+1] == CRGB::White;
+
+        CHSV main_hsv = HSV_leds[i];
+        HSV_leds[i+1] = main_hsv;
+        for (int32_t j = i; (j > i - COMET_SIZE) && (j >= 0); j--)
+        {
+          leds[j] = CHSV(main_hsv.hue, min(main_hsv.saturation + (uint8_t)random(50),255), max(main_hsv.value - (uint8_t)random(40), 0));
+        }
+      }
+    }
+    
+    break;
+  
+  default:
+    // normal whac a mole operation
+    break;
+  }
+
+  led_controller->setLeds(led_head, NUM_LEDS);
 }
 
 void setup() {
   Serial.begin(9600);
   Serial.println("We started");
-  state = IDLE;
+  state = BASIC_INTERACTION;
   FastLED.setBrightness(100);
   animation_array_construction(leds);
   led_controller = &FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
@@ -116,6 +165,7 @@ void setup() {
   }
 
   popTime = 0;
+  first_time = true;
 }
 
 void loop() {
@@ -123,7 +173,7 @@ void loop() {
   switch (state)
   {
     case IDLE:
-      idle2();
+      idle();
       break;
     
     case GAME_START:
@@ -144,7 +194,14 @@ void loop() {
 
       break;
     
+    case BASIC_INTERACTION:
+
+      basicInteraction();
+
+      break;
+
     default:
       break;
     }
+  updateLedstrip();
 }
