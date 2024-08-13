e3aacc672822989fee4ad4f09fc515d97583b31e
GerbenKoopmans <gerbn01@gmail.com>
2024-08-13 22:36:00 +0200

    comments toevoegen + logica schrijven

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,40 @@
 #include <Arduino.h>
 #include <FastLED.h>
-#include <ADS1X15.h>
 
-#define NUM_LEDS_STRIP 180
-#define NUM_LEDS 540
+// LED beam config
+#define NUM_LEDS_STRIP_3M 180
+#define NUM_LEDS_STRIP_5M 300
+#define NUM_STRIPS 4
+#define NUM_LEDS 960
 #define DATA_PIN 33
 #define DATA_PIN_2 32
 #define DATA_PIN_3 25
+#define DATA_PIN_4
+#define DATA_PIN_5
 #define COLOR_ORDER GRB
-#define CHIPSET  WS2812B
+#define CHIPSET WS2812B
 
-#define COOLDOWN_TIME_MOLE 10
-#define MAX_COMETS 2
-#define LED_SKIPS 3
+// Inputs and outputs
+#define NUM_BEAMS 5
+#define NUM_SENSORS 5
+uint8_t beams[NUM_BEAMS];
+uint8_t sensors[NUM_SENSORS];   // Sensors
+int sensor_values[NUM_SENSORS]; // Values
+
+// Game variables
+#define MAX_COMETS 2 // ?
+#define LED_SKIPS 3  // ?
 #define COMET_SIZE 20
 #define SENSOR_THRESHOLD 400
+#define SIMON_PATTERN_LENGTH 200
+uint8_t simon_says[SIMON_PATTERN_LENGTH];
+uint8_t simon_increment = 0;
+uint8_t simon_pattern_length = 0;
 
-#define SDA_1 21
-#define SCL_1 22
+// Timer to reset to idle
+unsigned long reset_timer = 0;
+unsigned long reset_delay = 10000; // 10 sec
 
-
-ADS1115 Sensor(0x48);
-
-int sensor_value;
 int moleActive;
 int notPopped;
 long long popUpTimeStamp;
@@ -30,218 +42,245 @@ long long popTime;
 long long cooldown_timer_mole;
 int first_time;
 
-
-CRGB leds[NUM_LEDS*3] = {0};
-CHSV HSV_leds[NUM_LEDS*3] = {CHSV(0,0,0)};
-int hue_comet[NUM_LEDS*3] = {0};
+CRGB leds[NUM_LEDS * 3] = {0};
+CHSV HSV_leds[NUM_LEDS * 3] = {CHSV(0, 0, 0)};
+int hue_comet[NUM_LEDS * 3] = {0};
 int value_comet[COMET_SIZE] = {0};
 
-CLEDController* led_controller; 
+CLEDController *led_controller;
 
-enum STATE {IDLE, GAME_START, POPPING_MOLES, ADD_SCORE, BASIC_INTERACTION};
+enum STATE
+{
+    IDLE,
+    ACTIVE,
+    GAME_START,
+    GAME_OVER,
+    BASIC_INTERACTION
+};
 STATE state;
 
+// Helper function for LED animation
 int value_array_construction()
 {
-  for (int i = 0; i < COMET_SIZE; i++)
-  {
-    value_comet[i] = COMET_SIZE - map(i, COMET_SIZE, 0, 255, 0);
-  }
-  
+    for (int i = 0; i < COMET_SIZE; i++)
+    {
+        value_comet[i] = COMET_SIZE - map(i, COMET_SIZE, 0, 255, 0);
+    }
 }
 
-int animation_array_construction(CRGB *led_array){
-	
-	for (int i = 0; i < NUM_LEDS; i++){
-		led_array[NUM_LEDS+i] = CRGB::Red;
-	}
-  return 0;
-}
-
-void resetMole(){
-  moleActive = true;
-  notPopped = true;
-  popUpTimeStamp = millis();
-}
-
-int idle()
+// Helper function for LED animation
+int animation_array_construction(CRGB *led_array)
 {
-  
-  CRGB* led_head = &leds[NUM_LEDS];
-	long long led_timer = micros();
 
-  for(int i = 0; i < NUM_LEDS; i++){
-    
-    led_head += 1;
-    led_controller->setLeds(led_head, NUM_LEDS);
-    FastLED.show();
-    Serial.println(micros() - led_timer);
-    led_timer = micros();
-	}
-  return 0;
-	//mooie idle animatie die checkt op activiteit
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        led_array[NUM_LEDS + i] = CRGB::Red;
+    }
+    return 0;
+}
+
+int idleAnimation()
+{
+    // Wat doet dit allemaal? Is dit nodig voor idle?
+    CRGB *led_head = &leds[NUM_LEDS];
+    long long led_timer = micros();
+
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+
+        led_head += 1;
+        led_controller->setLeds(led_head, NUM_LEDS);
+        FastLED.show();
+        Serial.println(micros() - led_timer);
+        led_timer = micros();
+    }
+    return 0;
+    // mooie idle animatie die checkt op activiteit
+}
+
+int getInputData(int sensor_num)
+{
+    // Pin HIGH so data comes in, start reading all pins
 }
 
 // Reads the sensor value and compares it to a threshold value. When the threshold has been surpassed and the cooldown timer is not active the a cooldown timer will be set to the defined COOLDOWN_TIME. When the cooldown timer is active the function returns true00
-int readIfMoleHit(int threshold){
-  long time_begin = millis();
-  int ADC_value = abs(Sensor.readADC_Differential_0_1());
-  Serial.println(ADC_value);
-  if (ADC_value > threshold && cooldown_timer_mole < millis())
-  {
-    cooldown_timer_mole = millis() + COOLDOWN_TIME_MOLE;
-  }
+int readIfMoleHit(int threshold)
+{
+    // Check all sensors
+    for (int i = 0; i < NUM_SENSORS; i++)
+    {
+        // If data comes in
+        if (getInputData(sensors[i]) > 0)
+        {
+            // If game is not being played
+            if (state == IDLE)
+            {
+                // Start game
+                state = GAME_START;
+                return 0;
+            }
+            // If game is being played
+            else if (state == ACTIVE)
+            {
+                // If correct mole is hit
+                if (correctMoleHit(i))
+                {
+                    // Nieuwe led aanzetten
+                }
+                else
+                {
+                    state = GAME_OVER;
+                }
+            }
+            // Return what mole is hit
+            return i;
+        }
+    }
+}
 
-  if (cooldown_timer_mole > millis())
-  {
-  // Serial.println(millis()-time_begin);
-    return true;
-  }
-  // Serial.println(millis()-time_begin);
-  return false;
+boolean correctMoleHit(int sensor)
+{
+    if (sensor == simon_says[simon_increment])
+    {
+        return true;
+    }
+    return false;
+}
+
+boolean timeIsUp()
+{
+    if (reset_delay < millis() - reset_timer)
+    {
+        reset_timer = millis();
+        return true;
+    }
+    return false;
 }
 
 int gameStart()
 {
-	//zet 1 licht aan en wacht op de gebruiker, als dit te lang duurt terug naar idle
-  return 0;
+    // zet 1 licht aan en wacht op de gebruiker, als dit te lang duurt terug naar idle
+    return 0;
 }
 
 int popingMoles()
 {
-  if(readIfMoleHit(SENSOR_THRESHOLD) && notPopped){
-    moleActive = false;
-    notPopped = false;
-    popTime = millis() - popUpTimeStamp;
-  }
-  return 0;
-  
+    if (readIfMoleHit(SENSOR_THRESHOLD) && notPopped)
+    {
+        moleActive = false;
+        notPopped = false;
+        popTime = millis() - popUpTimeStamp;
+    }
+    return 0;
 
-	//hoofdstate van het spel, mollen komen omhoog en wachten om gesmackt te worden.
-}
-
-int addScore()
-{
-	//als de speler succesvol is in het smacken van de mole wordt deze score toegevoegd aan de score verwerkt in de ledstrips
-  return 0;
+    // hoofdstate van het spel, mollen komen omhoog en wachten om gesmackt te worden.
 }
 
 void basicInteraction()
 {
-  if(readIfMoleHit(SENSOR_THRESHOLD) && first_time)
-  {
-    hue_comet[0] = random(256);
-    first_time = false;
-  }  
-  else
-  {
-    if (!readIfMoleHit(SENSOR_THRESHOLD))
+    if (readIfMoleHit(SENSOR_THRESHOLD) && first_time)
     {
-      first_time = true;
-    }   
-  }
+        hue_comet[0] = random(256);
+        first_time = false;
+    }
+    else
+    {
+        if (!readIfMoleHit(SENSOR_THRESHOLD))
+        {
+            first_time = true;
+        }
+    }
 }
 
 void updateLedstrip()
 {
-  CRGB* led_head = &leds[0];
+    CRGB *led_head = &leds[0];
 
-  switch (state)
-  {
-  case BASIC_INTERACTION:
-  {
-    uint32_t virtual_beam_size = NUM_LEDS + COMET_SIZE;
-
-    for (int32_t i = virtual_beam_size; i >= 0; i--)
+    switch (state)
     {
-      if (hue_comet[i] != 0)
-      {
-        int current_hue_comet = hue_comet[i];
-        hue_comet[i+LED_SKIPS] = current_hue_comet;
-        hue_comet[i] = 0;
-
-        for (int j = i; (j > i - COMET_SIZE) && (j >= 0); j--)
-        {
-          leds[j] = CHSV(current_hue_comet, min(COMET_SIZE-((i-j)*12)-(int)random(50),50), max(200 - (int)random(40), 0));
-        }
-
-        if(i - COMET_SIZE >= 0)
-        {
-          for (int j = 0; j < LED_SKIPS; j++)
-          {
-            leds[i-COMET_SIZE-j] = CRGB::Black;
-          }          
-        }        
-      }
-    }
-    
-  }
-  break;
-
-  default:
-    // normal whac a mole operation
-    break;
-  }
-  led_controller->setLeds(led_head, NUM_LEDS);
-  FastLED.show();
-}
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("We started");
-  state = BASIC_INTERACTION;
-  FastLED.setBrightness(100);
-  setCpuFrequencyMhz(240);
-  animation_array_construction(leds);
-  // value_array_construction();
-  // led_controller = &FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-  led_controller = &FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, 0, NUM_LEDS_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<CHIPSET, DATA_PIN_2, COLOR_ORDER>(leds, 180, NUM_LEDS_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<CHIPSET, DATA_PIN_3, COLOR_ORDER>(leds, 360, NUM_LEDS_STRIP).setCorrection(TypicalLEDStrip);
-  if(!Sensor.begin()){
-    Serial.println("Ja dat werkt niet; Sensor Fault can't connect");
-  }
-  Sensor.setDataRate(7);
-
-  popTime = 0;
-  first_time = true;
-}
-
-void loop() {
-
-  switch (state)
-  {
-    case IDLE:
-      idle();
-      break;
-    
-    case GAME_START:
-      
-      gameStart();
-
-      break;
-    
-    case POPPING_MOLES:
-      
-      popingMoles();
-
-      break;
-    
-    case ADD_SCORE:
-      
-      addScore();
-
-      break;
-    
     case BASIC_INTERACTION:
+    {
+        uint32_t virtual_beam_size = NUM_LEDS + COMET_SIZE;
 
-      basicInteraction();
+        for (int32_t i = virtual_beam_size; i >= 0; i--)
+        {
+            if (hue_comet[i] != 0)
+            {
+                int current_hue_comet = hue_comet[i];
+                hue_comet[i + LED_SKIPS] = current_hue_comet;
+                hue_comet[i] = 0;
 
-      break;
+                for (int j = i; (j > i - COMET_SIZE) && (j >= 0); j--)
+                {
+                    leds[j] = CHSV(current_hue_comet, min(COMET_SIZE - ((i - j) * 12) - (int)random(50), 50), max(200 - (int)random(40), 0));
+                }
+
+                if (i - COMET_SIZE >= 0)
+                {
+                    for (int j = 0; j < LED_SKIPS; j++)
+                    {
+                        leds[i - COMET_SIZE - j] = CRGB::Black;
+                    }
+                }
+            }
+        }
+    }
+    break;
 
     default:
-      break;
+        // normal whac a mole operation
+        break;
     }
-    
-  updateLedstrip();
+    led_controller->setLeds(led_head, NUM_LEDS);
+    FastLED.show();
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("We started");
+    state = BASIC_INTERACTION;
+    FastLED.setBrightness(100);
+    setCpuFrequencyMhz(240);
+    animation_array_construction(leds);
+    // value_array_construction();
+    // led_controller = &FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+    led_controller = &FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, 0, NUM_LEDS_STRIP).setCorrection(TypicalLEDStrip);
+    FastLED.addLeds<CHIPSET, DATA_PIN_2, COLOR_ORDER>(leds, 180, NUM_LEDS_STRIP).setCorrection(TypicalLEDStrip);
+    FastLED.addLeds<CHIPSET, DATA_PIN_3, COLOR_ORDER>(leds, 360, NUM_LEDS_STRIP).setCorrection(TypicalLEDStrip);
+
+    popTime = 0;
+    first_time = true;
+}
+
+void loop()
+{
+
+    switch (state)
+    {
+    case IDLE:
+        readIfMoleHit(SENSOR_THRESHOLD);
+        idleAnimation();
+        break;
+
+    case ACTIVE:
+        readIfMoleHit(SENSOR_THRESHOLD);
+
+        if (timeIsUp())
+        {
+            state = GAME_OVER;
+        }
+        break;
+
+    case BASIC_INTERACTION:
+
+        basicInteraction();
+
+        break;
+
+    default:
+        break;
+    }
+
+    updateLedstrip();
 }
